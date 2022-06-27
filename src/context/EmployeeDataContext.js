fd7c0bc558fd85406f0d7ae1db95d4b79f49d0ec
Enes ÇEB <enesceb@hotmail.com>
2022-06-27 11:09:45 +0300

    DataContext and Fetch API base created

---
+++ src/context/EmployeeDataContext.js
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+import useFetchUsers from "../services/useFetchUsers";
+
+
+
+export const DataContext = createContext();
+
+export const DataProvider = ({ children }) => {
+    
+    const { users } = useFetchUsers();
+
+    return (
+        <DataContext.Provider value={{users}}>
+            {children}
+        </DataContext.Provider>
+    )
+}
