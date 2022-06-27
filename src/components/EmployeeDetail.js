e3c4c74cfba1fe627eaf82ee97458c623cbcfa31
Enes ÇEB <enesceb@hotmail.com>
2022-06-27 16:28:21 +0300

    ProfileDetal Component's styles improved. Context implemented date-fns dependency installed for date formatting

--- src/components/EmployeeDetail.js
+++ src/components/EmployeeDetail.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { DataContext } from '../context/DataContext'
+import { format } from "date-fns";
+
 
 const EmployeeDetail = () => {
+    const { users } = useContext(DataContext);
+    const { name:{title, first,last}, picture:{large}, phone, cell, email, location, dob:{ date, age}, gender, nat} = users[0];
+    console.log(users)
+    const formatedDOB = format(new Date(date), "MM/dd/yyyy")
+
   return (
     <div className="container-profile">
     <div className="card-container-profile">
@@ -8,23 +16,30 @@ const EmployeeDetail = () => {
         <div className="avatar-container-profile">
           <a href="/">
             <div className="avatar-profile">
-              <img src='https://randomuser.me/api/portraits/men/75.jpg'/>
+            <img src={large} alt={`${first}-${last}`}/>
             </div>
           </a>
         </div>
         <div className='card'>
-        <h2 className='card-title-profile'>AD</h2>
+        <h2 className='card-title-profile'>{title} {first} {last}</h2>
         </div>
         <div className='card-body-profile'>
         <div>
-          <h2 className="card-title-profile"> </h2>
-          <p className="card-text-profile">Email : </p>
-          <p className="card-text-profile " style={{textTransform: "uppercase"}}>Phone :</p>
-          <p className="card-text-profile " style={{textTransform: "uppercase"}}>Age :</p>
+          <p className="card-text-profile">Email: {email} </p>
+          <p className="card-text-profile " >Phone: {phone}</p>
+          <p className="card-text-profile " >Age: {age}</p>
+        </div>
+        <div>
+          
+          <p className="card-text-profile">Gender: {gender} </p>
+          <p className="card-text-profile ">Nationalities: {nat}</p>
+          <p className="card-text-profile ">Birtday: {formatedDOB} </p>
         </div>
         </div>
-        <div >
-          <p className="card-text-profile">Adres : </p>
+        <div className='card' style={{alignItems: "baseline"}}>
+        <h3 style={{marginRight: "10px"}}>Location:</h3> 
+        <p className="card-text-profile">  {location.street.number} {location.street.name}. {location.city} </p>
+        <p className="card-text-profile">{location.state} {location.postcode} </p>
         </div>
         <div className="card-footer-profile">
       
