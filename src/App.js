07bcfe6b045c828245e4a0c1b09783b18934baba
Enes ÇEB <enesceb@hotmail.com>
2022-06-27 12:16:16 +0300

    renamed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { DataProvider } from "./context/EmployeeDataContext";
+import { DataProvider } from "./context/DataContext";
 import Home from "./pages/Home";
 
 function App() {
